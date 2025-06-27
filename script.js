62cd779b0d725eaa50708a18dbf9811c39b7695b
Mathieu762005 <mathieu762005@gmail.com>
2025-06-27 17:47:19 +0200

    mise a jour

--- script.js
+++ script.js
@@ -1,68 +1,77 @@
-const motsPendu = ["café", "binouze", "cailloux"]
-let motAdeviner = motsPendu[Math.floor(Math.random() * (motsPendu.length + 1))]
-motAdeviner = motAdeviner.toUpperCase()
+// Ma liste de mots
+const motsPendu = ["cafe", "binouze", "cailloux"]
+
+// Pour Stack mon mot aleatoire en majuscule
+let motAdeviner = motsPendu[Math.floor(Math.random() * (motsPendu.length))].toUpperCase()
+
+// Pour stack mon choix de lettre
+let choixUser = ""
+
+// pour stack mes emplacement
+const underscore = []
+
+let erreur = 0
+
 console.log(motAdeviner)
-let choixUser = 0
 
-function recupLettres(choix) {
-    choixUser = choix
-    console.log(choixUser)
+
+// Placer les underscores par rapport au nombre de lettres du mot
+for (let i = 0; i < motAdeviner.length; i++) {
+    underscore.push("_");
 }
 
-for (i = 0; i < motAdeviner.length; i++) {
-    document.getElementById("lettrePendu").innerText += " _ " 
+// la function pour le jeux
+function recupLettres(choixUser) {
+    const lettreMot = motAdeviner.split("")
+
+    if (lettreMot.includes(choixUser)) {
+        for (i = 0; i < lettreMot.length; i++) {
+            // Si lettreMot scan par la boucle = le choix du joueur
+            if (lettreMot[i] === choixUser) {
+                // Alors underscore scan par la boucle prend la valeur de choix User
+                underscore[i] = choixUser
+            }
+        }
+    } else {
+        erreur++
+        document.getElementById("pendu").innerHTML = `<img src="img/1.PNG" alt=""></img>`
+        if (erreur == 2) {
+            document.getElementById("pendu").innerHTML = `<img src="img/2.PNG" alt=""></img>`
+        } else if (erreur == 3) {
+            document.getElementById("pendu").innerHTML = `<img src="img/3.PNG" alt=""></img>`
+        } else if (erreur == 4) {
+            document.getElementById("pendu").innerHTML = `<img src="img/4.PNG" alt=""></img>`
+        } else if (erreur == 5) {
+            document.getElementById("pendu").innerHTML = `<img src="img/5.PNG" alt=""></img>`
+        } else if (erreur == 6) {
+            document.getElementById("pendu").innerHTML = `<img src="img/6.PNG" alt=""></img>`
+        } else if (erreur == 7) {
+            document.getElementById("pendu").innerHTML = `<img src="img/7.PNG" alt=""></img>`
+        } else if (erreur == 8) {
+            document.getElementById("pendu").innerHTML = `<img src="img/8.PNG" alt=""></img>`
+        } else if (erreur == 9) {
+            document.getElementById("pendu").innerHTML = `<img src="img/9.PNG" alt=""></img>`
+            setTimeout(() => {
+                document.getElementById("pendu").innerHTML = `<img src="img/Game Over GIF.gif" id="refresh" alt=""></img>`
+            }, 1500);
+            setTimeout(() => {
+                // On ajoute le bouton dans le DOM
+                document.getElementById("pendu").innerHTML = `<input type="button" value="Rafraichir" id="refresh" />`;
+
+                // Ensuite, on récupère le bouton fraichement créé
+                let refresh = document.getElementById("refresh");
+
+                // Et on ajoute le bon event listener
+                refresh.addEventListener('click', () => {
+                    location.reload();
+                });
+            }, 2500);
+        }
+    }
+    document.getElementById("lettrePendu").innerText = underscore.join(" ");
+
+    console.log(erreur)
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function jeux() {
-//     if (choixUser == motAdeviner.includes()) {
-//         document.getElementById("lettrePendu").innerHTML = choixUser
-//     } else {
-
-//     }
-// }
-
-
-// } else if (i == 0) {
-//     document.getElementById("pendu").innerHTML = `<img src="img/1.PNG" alt=""></img>`
-// } else if (i == 1) {
-//     document.getElementById("pendu").innerHTML = `<img src="img/2.PNG" alt=""></img>`
-// } else if (i == 2) {
-//     document.getElementById("pendu").innerHTML = `<img src="img/3.PNG" alt=""></img>`
-// } else if (i == 3) {
-//     document.getElementById("pendu").innerHTML = `<img src="img/4.PNG" alt=""></img>`
-// } else if (i == 4) {
-//     document.getElementById("pendu").innerHTML = `<img src="img/5.PNG" alt=""></img>`
-// } else if (i == 5) {
-//     document.getElementById("pendu").innerHTML = `<img src="img/6.PNG" alt=""></img>`
-// } else if (i == 6) {
-//     document.getElementById("pendu").innerHTML = `<img src="img/7.PNG" alt=""></img>`
-// } else if (i == 7) {
-//     document.getElementById("pendu").innerHTML = `<img src="img/8.PNG" alt=""></img>`
-// } else if (i == 8) {
-//     document.getElementById("pendu").innerHTML = `<img src="img/9.PNG" alt=""></img>`
-// }
-
-
-
-
-
-
-
 
 
 
